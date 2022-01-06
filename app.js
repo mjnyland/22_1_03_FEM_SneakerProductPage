fdd946d19ff042109eec2efbaa293111a2a1a144
mjnyland <mjnyland@gmail.com>
2022-01-06 06:48:22 -0600

    Basic MVC working. Beginning styling.

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
+class View{
 
-
-
-
-
-
-
-
-class Cart{
-    constructor(items = []){
-        this.items = items;
+    constructor(controller, container){
+        this.controller = controller;
+        this.container = container;
     }
 
-    addItem(item){
-        this.items.push(item)            
+    presentData(obj) {
+        this.container.innerHTML = `
+            <img class="sneaker-image" src="${obj.images.productImgs.jpgPath1}" alt="">
+            <h3 class="sneaker-brand">${obj.brand}</h3>
+            <h1 class="sneaker-name">${obj.name}</h1>
+            <p class="sneaker-descrip">${obj.description}</p>
+            <h1 class="sneaker-sale-price">$${obj.salePrice}</h1>
+            <h3 class="sneaker-sale">${obj.sale}%</h3>
+            <h3 class="sneaker-original-price">$${obj.originalPrice.toFixed(2)}</h3>
+        `
     }
 }
 
+class Model{
+    constructor(items){
+        this.items = items;
+    }
 
+    get sneakerData(){
+        return this.items.sneaker;
+    }
+}
+
+class Controller{
+    constructor(model){
+        this.model = model;
+    }
+
+    //METHOD: Event Listener Interface
+
+    get HTML() {
+        view.presentData(model.sneakerData);
+    }
+
+
+}
 
 class Sneaker{
     constructor(images, name, brand, description, originalPrice, sale = 0, quantity = 0){
@@ -33,8 +57,8 @@ class Sneaker{
         this._sale = sale;
         const dollarsOff = this.originalPrice * (sale/100);
         const priceNum = this.originalPrice - dollarsOff;
+
         this.salePrice = priceNum.toFixed(2); 
-        //this.totalPrice = this.salePrice * this.quantity;
     }
 
     get sale(){
@@ -43,11 +67,7 @@ class Sneaker{
 
     set quantity(quantity){
         this._quantity = quantity;
-        if (quantity <= 0){
-            this.totalPrice = 0
-        } else {
-            this.totalPrice = this.salePrice * this.quantity;
-        }
+        this.totalPrice = this.salePrice * quantity;
     }
 
     get quantity(){
@@ -55,74 +75,41 @@ class Sneaker{
     }
 
     addOne() {
-        console.log('+1')
         this.quantity += 1;
         displaySneaker();
     }
 }
 
-const productImgs = {
-    jpgPath1: '/ecommerce-product-page-main/images/image-product-1.jpg',
-    jpgPath2: '/ecommerce-product-page-main/images/image-product-2.jpg', 
-    jpgPath3: '/ecommerce-product-page-main/images/image-product-3.jpg',
-    jpgPath4: '/ecommerce-product-page-main/images/image-product-4.jpg' 
-};
-
-const thumbnails = {
-    jpgPath1: '/ecommerce-product-page-main/images/image-product-1-thumbnail.jpg',
-    jpgPath2: '/ecommerce-product-page-main/images/image-product-2-thumbnail.jpg',
-    jpgPath3: '/ecommerce-product-page-main/images/image-product-3-thumbnail.jpg',
-    jpgPath4: '/ecommerce-product-page-main/images/image-product-4-thumbnail.jpg' 
-};
-
-
-const fallLmtdEditSneaker = new Sneaker(
-[productImgs, thumbnails],
-'Fall Limited Edition Sneakers',
-'SNEAKER COMPANY',
-`These low-profile sneakers are your perfect casual wear companion. Featuring a durable
-rubber outer sole, they'll withstand everything the weather can offer.`,
-250)
-
-function loadSales(){
-    fallLmtdEditSneaker.sale = 50;
+const sneakerCont = document.querySelector('.sneaker-container');
+const images = {
+        productImgs: {
+            jpgPath1: '/ecommerce-product-page-main/images/image-product-1.jpg',
+            jpgPath2: '/ecommerce-product-page-main/images/image-product-2.jpg', 
+            jpgPath3: '/ecommerce-product-page-main/images/image-product-3.jpg',
+            jpgPath4: '/ecommerce-product-page-main/images/image-product-4.jpg' 
+        },
+        thumbnails: {
+            jpgPath1: '/ecommerce-product-page-main/images/image-product-1-thumbnail.jpg',
+            jpgPath2: '/ecommerce-product-page-main/images/image-product-2-thumbnail.jpg',
+            jpgPath3: '/ecommerce-product-page-main/images/image-product-3-thumbnail.jpg',
+            jpgPath4: '/ecommerce-product-page-main/images/image-product-4-thumbnail.jpg' 
+        }
 }
-
-function displaySneaker(){
-    const sneakerCont = document.querySelector('.sneaker-container');
-    console.log(fallLmtdEditSneaker)
-    sneakerCont.innerHTML = `
-        <img src="${fallLmtdEditSneaker.images[0].jpgPath1}" alt="Image-product" class="displayed-image">
-        <h3 class="sneaker-brand">${fallLmtdEditSneaker.brand}</h3>
-        <h1 class="sneaker-name">${fallLmtdEditSneaker.name}</h1>
-        <p class="sneaker-descrip">${fallLmtdEditSneaker.description}</p>
-        <h1 class="price">$${fallLmtdEditSneaker.salePrice}</h1>
-        <p class="sale">${fallLmtdEditSneaker.sale}%</p>
-        <p class="original-price">${fallLmtdEditSneaker.originalPrice}</p>
-
-        <div class="quantity-button">
-            <img src="/ecommerce-product-page-main/images/icon-minus.svg" alt="Minus Icon" class="minus-icon">
-            <p>${fallLmtdEditSneaker._quantity}</p>
-            <img src="/ecommerce-product-page-main/images/icon-plus.svg" class="plus-icon" alt="Plus Icon">
-        </div>
-
-        <div class="add-to-cart">
-            <img src="/ecommerce-product-page-main/images/icon-cart.svg" alt="Cart Icon">
-            <p></p>
-        </div>
-    `;
-}
+const sneaker = new Sneaker(
+    images,
+    'Fall Limited Edition Sneakers',
+    'SNEAKER COMPANY',
+    `These low-profile sneakers are your perfect casual wear companion. Featuring a durable
+    rubber outer sole, they'll withstand everything the weather can offer.`,
+    250);
 
 
 
-loadSales();
-displaySneaker();
+var model = new Model({sneaker});
+var controller = new Controller(model);
+var view = new View(controller, sneakerCont);
 
-const plusIcon = document.querySelector('.plus-icon')
+console.log(model)
 
-
-plusIcon.addEventListener('click', e => {
-    //if(e.target.className === "plus-icon"){
-        fallLmtdEditSneaker.addOne();
-    //}
-})
+sneaker.sale = 50;
+controller.HTML;
